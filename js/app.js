0519f4787c60e6cf37af870718cc4451357c004c
Jonathan Simon Prates <jonathan.simonprates@gmail.com>
2015-04-11 18:42:21 -0300

    Added sliding menu

--- js/app.js
+++ js/app.js
@@ -2,57 +2,89 @@ var app = angular.module('app138', ['onsen.directives']);
 
 app.controller('chat', function($scope, $rootScope) {
 
+  $scope.people = [{
+    "name": "fulano"
+  }]
+
   $scope.items = [{
+      "from": "usuario",
+      "msg": "Essa é a minha mensagem",
+      "type": "message",
+      "avatar": "http://www.avatarsdb.com/avatars/selly_gomez.jpg"
+    }, {
+      "from": "usuario",
+      "msg": "Essa é a minha mensagem",
+      "type": "message",
+      "avatar": "http://www.avatarsdb.com/avatars/rihanna_rebelle_fleur.gif"
+    }, {
+      "from": "usuario",
+      "msg": "Essa é a minha mensagem",
+      "type": "message",
+      "avatar": "http://www.avatarsdb.com/avatars/i_see_dumb_people.jpg"
+    }, {
       "from": "me",
       "msg": "Essa é a minha mensagem",
-      "type": "message"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message"
+      "type": "message",
+      "avatar": "http://www.avatarsdb.com/avatars/pc_user.gif"
     }, {
       "from": "me",
       "msg": "Essa é a minha mensagem",
-      "type": "message"
+      "type": "message",
+      "avatar": "http://www.avatarsdb.com/avatars/pc_user.gif"
+    }, {
+      "from": "usuario",
+      "msg": "Essa é a minha mensagem",
+      "type": "message",
+      "avatar": "  http://www.avatarsdb.com/avatars/rihanna_new_ps.jpg"
+    }, {
+      "from": "me",
+      "msg": "Essa é a minha mensagem",
+      "type": "message",
+      "avatar": "http://www.avatarsdb.com/avatars/pc_user.gif"
+    }, {
+      "from": "usuario",
+      "msg": "Essa é a minha mensagem",
+      "type": "message",
+      "avatar": "http://www.avatarsdb.com/avatars/rihanna_rebelle_fleur.gif"
+    }, {
+      "from": "me",
+      "msg": "Essa é a minha mensagem",
+      "type": "message",
+      "avatar": "http://www.avatarsdb.com/avatars/pc_user.gif"
+    }, {
+      "from": "usuario",
+      "msg": "Essa é a minha mensagem",
+      "type": "message",
+      "avatar": "http://www.avatarsdb.com/avatars/rihanna_rebelle_fleur.gif"
+    }, {
+      "from": "usuario",
+      "msg": "Essa é a minha mensagem",
+      "type": "message",
+      "avatar": "http://www.avatarsdb.com/avatars/rihanna_rebelle_fleur.gif"
+    }, {
+      "from": "usuario",
+      "msg": "Essa é a minha mensagem",
+      "type": "message",
+      "avatar": "http://www.avatarsdb.com/avatars/vibrating_cat.gif"
     },
-
-    {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message"
-    },
-
-
 
   ];
+
+  $scope.login = function(){
+    app.slidingMenu.setSwipeable(true);
+    app.slidingMenu.setMainPage("page.html");
+  };
+
+  $scope.logout = function(){
+    app.slidingMenu.closeMenu();
+    app.slidingMenu.setSwipeable(false);
+    app.slidingMenu.setMainPage("login.html");
+  };
+
+  $scope.sendTo = function(name) {
+    $("#fieldMessage").val("@" + name + " ");
+    $("#fieldMessage").focus();
+  }
 
 
   $scope.sendMessage = function(field) {
@@ -60,7 +92,8 @@ app.controller('chat', function($scope, $rootScope) {
     $scope.items.unshift({
       "from": "me",
       "msg": field,
-      "type": "message"
+      "type": "message",
+      "avatar": "http://www.avatarsdb.com/avatars/vibrating_cat.gif"
     });
   }
 
