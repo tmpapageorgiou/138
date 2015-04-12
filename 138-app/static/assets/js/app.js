7fe1d0e78ebf8b92d6b2aa593a04813d417ea907
Jonathan Simon Prates <jonathan.simonprates@gmail.com>
2015-04-11 23:01:39 -0300

    Backend + Frontend

--- 138-app/static/assets/js/app.js
+++ 138-app/static/assets/js/app.js
@@ -1,84 +1,35 @@
 var app = angular.module('app138', ['onsen.directives']);
 
-app.controller('chat', function($scope, $rootScope) {
+app.controller('chat', function($scope, $rootScope, $timeout) {
 
-  $scope.people = [{
-    "name": "fulano"
-  }]
+  $scope.people = [];
+  $scope.messages = [];
 
-  $scope.items = [{
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message",
-      "avatar": "http://www.avatarsdb.com/avatars/selly_gomez.jpg"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message",
-      "avatar": "http://www.avatarsdb.com/avatars/rihanna_rebelle_fleur.gif"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message",
-      "avatar": "http://www.avatarsdb.com/avatars/i_see_dumb_people.jpg"
-    }, {
-      "from": "me",
-      "msg": "Essa é a minha mensagem",
-      "type": "message",
-      "avatar": "http://www.avatarsdb.com/avatars/pc_user.gif"
-    }, {
-      "from": "me",
-      "msg": "Essa é a minha mensagem",
-      "type": "message",
-      "avatar": "http://www.avatarsdb.com/avatars/pc_user.gif"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message",
-      "avatar": "  http://www.avatarsdb.com/avatars/rihanna_new_ps.jpg"
-    }, {
-      "from": "me",
-      "msg": "Essa é a minha mensagem",
-      "type": "message",
-      "avatar": "http://www.avatarsdb.com/avatars/pc_user.gif"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message",
-      "avatar": "http://www.avatarsdb.com/avatars/rihanna_rebelle_fleur.gif"
-    }, {
-      "from": "me",
-      "msg": "Essa é a minha mensagem",
-      "type": "message",
-      "avatar": "http://www.avatarsdb.com/avatars/pc_user.gif"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message",
-      "avatar": "http://www.avatarsdb.com/avatars/rihanna_rebelle_fleur.gif"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message",
-      "avatar": "http://www.avatarsdb.com/avatars/rihanna_rebelle_fleur.gif"
-    }, {
-      "from": "usuario",
-      "msg": "Essa é a minha mensagem",
-      "type": "message",
-      "avatar": "http://www.avatarsdb.com/avatars/vibrating_cat.gif"
-    },
+  $scope.api138 = new Api138({
+    userID: 'fulano',
+    host: '172.16.5.182',
+    port: 8888,
+    onMsgCallback: function(data) {
+      $scope.messages.unshift(data);
+      $scope.$apply();
+    }
+  });
 
-  ];
-
-  $scope.login = function(){
+  $scope.login = function() {
     app.slidingMenu.setSwipeable(true);
     app.slidingMenu.setMainPage("page.html");
+    $("#fieldMessage").removeAttr("readonly");
+    $("#fieldMessage").val("");
+    $scope.doAnimate = false;
   };
 
-  $scope.logout = function(){
+  $scope.logout = function() {
     app.slidingMenu.closeMenu();
     app.slidingMenu.setSwipeable(false);
-    app.slidingMenu.setMainPage("login.html");
+    app.slidingMenu.setMainPage("page.html");
+    $("#fieldMessage").addAttr("readonly");
+    $scope.doAnimate = true;
+    location.href('index.html');
   };
 
   $scope.sendTo = function(name) {
@@ -86,15 +37,43 @@ app.controller('chat', function($scope, $rootScope) {
     $("#fieldMessage").focus();
   }
 
+  $scope.field = "";
+  $scope.randomSentences = ["Conheça novas pessoas.",
+    "Converse pessoas pŕoximas a você.",
+    "Fale com pessoas no seu círculo de bate papo.",
+    "E aí, quer tc?"
+  ];
+  $scope.charIndex = 0;
+  $scope.sentenceIndex = 0;
+  $scope.currentSentences = $scope.randomSentences[0];
+  $scope.doAnimate = true;
+
+  $scope.animateText = function() {
+    $timeout(function() {
+
+      if ($scope.doAnimate) {
+        $scope.field = $scope.currentSentences.substring(0, $scope.charIndex);
+        $scope.charIndex++;
+        if ($scope.charIndex > $scope.currentSentences.length) {
+          $scope.sentenceIndex++;
+          if ($scope.sentenceIndex >= $scope.randomSentences.length) {
+            $scope.sentenceIndex = 0;
+          }
+          $scope.charIndex = 0;
+          $scope.currentSentences = $scope.randomSentences[$scope.sentenceIndex];
+        }
+      }
+      $scope.animateText();
+    }, 90);
+  };
+
+  //$scope.animateText();
+
 
   $scope.sendMessage = function(field) {
     $("#fieldMessage").val("");
-    $scope.items.unshift({
-      "from": "me",
-      "msg": field,
-      "type": "message",
-      "avatar": "http://www.avatarsdb.com/avatars/vibrating_cat.gif"
-    });
+    if (field != "") {
+      $scope.api138.sendMessage(field);
+    }
   }
-
 });
