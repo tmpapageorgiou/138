4eb2d068acd0b6dbe4f4cbf0529f50fda7742550
Cristian Oliveira <contato@cristianoliveira.com.br>
2015-04-12 02:30:52 -0300

    (__)

--- 138-app/static/assets/js/api/api-138.js
+++ 138-app/static/assets/js/api/api-138.js
@@ -1,112 +1,126 @@
-var Api138 = function(data) {
-  'use strict';
+var Api318 = function (data) {
+	'use strict';
 
 
-  var PRIVATE = {},
-    PUBLIC = this,
-    W = window,
-    D = document,
-    LOCATION = W.location,
-    HOSTNAME = LOCATION.hostname,
-    PORT = LOCATION.port,
-    CONNECTION;
+	var PRIVATE = {},
+		PUBLIC = this,
+		W = window,
+		D = document,
+		LOCATION = W.location,
+		HOSTNAME = LOCATION.hostname,
+		PORT = LOCATION.port,
+		CONNECTION;
 
 
-  /**
-   *
-   * PRIVATE METHODS
-   *
-   **/
-  PRIVATE.parse = function(str) {
-    return JSON.parse(str);
-  };
+	/**
+	*
+	* PRIVATE METHODS
+	*
+	**/
+	PRIVATE.parse = function (str) {
+		return JSON.parse(str);
+	};
 
-  PRIVATE.stringify = function(obj) {
-    return JSON.stringify(obj);
-  };
+	PRIVATE.stringify = function (obj) {
+		return JSON.stringify(obj);
+	};
 
-  PRIVATE.getURL = function() {
-    var hostname = data.host ? data.host : HOSTNAME,
-      port = data.port ? data.port : PORT;
+	PRIVATE.getURL = function () {
+		var hostname = data.hostname ? data.hostname : HOSTNAME,
+			port = data.port ? data.port : PORT;
 
-    return 'ws://' + hostname + (((port != 80) && (port != 443)) ? ':' + port : '') + '/ws/' + data.userID;
-  };
+		return 'ws://' + hostname + (((port != 80) && (port != 443)) ? ':' + port : '') + '/ws/' + data.userID;	
+	};
+
+	PRIVATE.getConnection = function(){
+
+	}
 
 
-  /**
-   *
-   * PUBLIC METHODS
-   *
-   **/
-  PUBLIC.sendMessage = function(str) {
-    var obj = {
-      type: 'message',
-      msg: String(str),
-      mentions: String(str).match(/@[\w]*/g)
+	/**
+	*
+	* PUBLIC METHODS
+	*
+	**/
+	PUBLIC.sendMessage = function (str) {
+		var obj = {
+				type: 'message',
+				msg: String(str),
+				mentions: String(str).match(/@[\w]*/g)
+			};
+
+		CONNECTION.send(PRIVATE.stringify(obj));
+	};
+
+
+	/**
+	*
+	* DEFINE URL AND SOCKET
+	*
+	**/
+	PRIVATE.connection = null;
+
+	CONNECTION = function(){
+		if (!PRIVATE.connection){
+			PRIVATE.connection = new WebSocket(PRIVATE.getURL());
+		}
+		return PRIVATE.connection
+	}
+
+	/**
+	*
+	* SEND GEOLOCATION
+	*
+	**/
+	if (navigator.geolocation) {
+		var lat,
+			lon,
+			geo;
+
+		setInterval(function(){
+			navigator.geolocation.getCurrentPosition(function (position) {
+			  	lat = position.coords.latitude;
+				lon = position.coords.longitude;
+				geo = JSON.stringify({latitude: lat, longitude: lon, type: 'position'});
+
+				CONNECTION.send(geo);
+			});
+		}, 5000);
+	} else {
+		alert('API Geolocation not found');
+	}
+
+
+	/**
+	*
+	* MESSAGES
+	*
+	**/
+	CONNECTION.onmessage = function(evt) {
+		console.log(evt);
+
+      	if (data.onMsgCallback) {
+      		data.onMsgCallback(evt);
+    	}
     };
 
-    CONNECTION.send(PRIVATE.stringify(obj));
-  };
-
-
-  /**
-   *
-   * DEFINE URL AND SOCKET
-   *
-   **/
-
-
-   CONNECTION = new WebSocket(PRIVATE.getURL());
-
-
-  /**
-   *
-   * SEND GEOLOCATION
-   *
-   **/
-  if (navigator.geolocation) {
-    var lat,
-      lon,
-      geo;
-
-    setInterval(function() {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        lat = position.coords.latitude;
-        lon = position.coords.longitude;
-        geo = JSON.stringify({
-          latitude: lat,
-          longitude: lon,
-          type: 'position'
-        });
-
-        CONNECTION.send(geo);
-      });
-    }, 5000);
-  } else {
-    alert('API Geolocation not found');
-  }
-
-
-  /**
-   *
-   * MESSAGES
-   *
-   **/
-  CONNECTION.onmessage = function(evt) {
-        if (data.onMsgCallback) {
-      data.onMsgCallback(JSON.parse(evt.data));
-    }
-  };
-
   CONNECTION.onclose = function(evt) {
-    console.log('Connection closed');
+    	console.log('Connection closed');
+			PRIVATE.connection = null;
   };
 
 
   /**
-   *
-   * RETURN ALL PUBLIC METHODS
-   *
-   **/
-  return PUBLIC;
+	*
+	* RETURN ALL PUBLIC METHODS
+	*
+	**/
+	return PUBLIC;
 };
+
+// var t = new Api318({
+// 	userID: 'fulano',
+// 	host: '172.16.5.182',
+// 	port: 8888,
+//  onMsgCallback:
+// });
